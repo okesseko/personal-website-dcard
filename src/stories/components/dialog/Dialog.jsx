d45114ebcb3765120c69e64c74d182651b0baf8a
JImmy <59650459+okesseko@users.noreply.github.com>
2022-03-14 00:43:59 +0800

    feat: add dialog component

---
+++ src/stories/components/dialog/Dialog.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react"
+import ReactDOM from "react-dom"
+import PropTypes from "prop-types"
+import "./dialog.scss"
+
+const Dialog = ({ children, dialogStyle, onClose, insertDomTree }) => {
+  useEffect(() => {
+    if (document) document.body.style.overflow = "hidden"
+    return () => {
+      if (document) document.body.style.overflow = null
+    }
+  }, [])
+
+  return ReactDOM.createPortal(
+    <div className="dialog" style={dialogStyle} onClick={onClose}>
+      {children}
+    </div>,
+    insertDomTree
+  )
+}
+
+Dialog.defaultProps = {
+  children: null,
+  dialogStyle: {},
+  onClose: () => {},
+  insertDomTree: document.body,
+}
+
+Dialog.propTypes = {
+  children: PropTypes.element,
+  dialogStyle: PropTypes.object,
+  onClose: PropTypes.func,
+  insertDomTree: PropTypes.element,
+}
+
+export default Dialog
---
+++ src/stories/components/dialog/Dialog.stories.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import Dialog from "./Dialog"
+import Button from "../button/Button"
+
+export default {
+  title: "Components/Dialog",
+  component: Dialog,
+}
+
+const Template = args => {
+  const [openDialog, setOpenDialog] = useState(false)
+
+  return (
+    <div>
+      <Button text="click me" onClick={() => setOpenDialog(true)} />
+      {openDialog && (
+        <Dialog {...args} onClose={() => setOpenDialog(false)}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              background: "white",
+              width: "200px",
+              height: "200px",
+              margin: "auto",
+            }}
+          >
+            Hi i am dialog
+          </div>
+        </Dialog>
+      )}
+    </div>
+  )
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  dialogStyle: { display: "flex" },
+  dialogContentStyle: {},
+}
