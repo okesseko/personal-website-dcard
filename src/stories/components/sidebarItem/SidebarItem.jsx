8ee974d0b568b21941dbcd179b0c0bbc2cc363a3
JImmy <59650459+okesseko@users.noreply.github.com>
2022-03-19 01:09:18 +0800

    feat: add new type "title" & "title" stytle

--- src/stories/components/sidebarItem/SidebarItem.jsx
+++ src/stories/components/sidebarItem/SidebarItem.jsx
@@ -2,17 +2,41 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./sidebarItem.scss"
 
-const SidebarItem = ({ IconComponent, iconSize, imageUrl, text, type }) => {
+const SidebarItem = ({
+  IconComponent,
+  iconSize,
+  imageUrl,
+  selected,
+  text,
+  type,
+}) => {
+  function shouldBeRenderedComponentByType() {
+    switch (type) {
+      case "icon":
+        return (
+          <div className="sidebarItem-icon">
+            <IconComponent size={iconSize} />
+          </div>
+        )
+
+      case "image":
+        return <img className="sidebarItem-image" src={imageUrl} />
+
+      default:
+        return null
+    }
+  }
+
   return (
-    <div className="sidebarItem">
-      {type === "icon" ? (
-        <div className="sidebarItem-icon">
-          <IconComponent size={iconSize} />
-        </div>
-      ) : (
-        <img className="sidebarItem-image" src={imageUrl} />
-      )}
-      <p className="sidebarItem-text">{text}</p>
+    <div className={`sidebarItem ${selected && "sidebarItem--selected"}`}>
+      {shouldBeRenderedComponentByType()}
+      <p
+        className={`sidebarItem-text ${
+          type === "title" && "sidebarItem-text--title"
+        }`}
+      >
+        {text}
+      </p>
     </div>
   )
 }
@@ -29,8 +53,9 @@ SidebarItem.propTypes = {
   IconComponent: PropTypes.elementType,
   iconSize: PropTypes.number,
   imageUrl: PropTypes.string,
+  selected: PropTypes.bool,
   text: PropTypes.string,
-  type: PropTypes.oneOf(["icon", "image"]),
+  type: PropTypes.oneOf(["icon", "image", "title"]),
 }
 
 export default SidebarItem
