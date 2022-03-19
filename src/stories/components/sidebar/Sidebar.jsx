84f48b491df7dacc1ce6e754fc63009726640f83
okesseko <59650459+okesseko@users.noreply.github.com>
2022-03-19 21:32:25 +0800

    feat: add onClick hanlder

--- src/stories/components/sidebar/Sidebar.jsx
+++ src/stories/components/sidebar/Sidebar.jsx
@@ -3,17 +3,17 @@ import PropTypes from "prop-types"
 import SidebarItem from "../sidebarItem/SidebarItem"
 import "./sidebar.scss"
 
-const Sidebar = ({ fixedItems, topicItems }) => {
+const Sidebar = ({ fixedItems, topicItems, onClick }) => {
   return (
     <div className="sidebar">
       <div className="sidebar__fixed">
         {fixedItems.map(item => (
-          <SidebarItem key={item.text} {...item} />
+          <SidebarItem key={item.value} onClick={onClick} {...item} />
         ))}
       </div>
       <div className="sidebar__topic">
         {topicItems.map(item => (
-          <SidebarItem key={item.text} {...item} />
+          <SidebarItem key={item.value} onClick={onClick} {...item} />
         ))}
       </div>
     </div>
@@ -23,11 +23,13 @@ const Sidebar = ({ fixedItems, topicItems }) => {
 Sidebar.defaultProps = {
   fixedItems: [],
   topicItems: [],
+  onClick: () => {},
 }
 
 Sidebar.propTypes = {
   fixedItems: PropTypes.arrayOf(PropTypes.shape(SidebarItem.propTypes)),
   topicItems: PropTypes.arrayOf(PropTypes.shape(SidebarItem.propTypes)),
+  onClick: PropTypes.func,
 }
 
 export default Sidebar
