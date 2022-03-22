f1c8e536ac84a809775dca9de5edd0e9e5c137ea
JImmy <59650459+okesseko@users.noreply.github.com>
2022-03-22 23:27:55 +0800

    feat: set router

--- wrapPageElement.js
+++ wrapPageElement.js
@@ -8,6 +8,13 @@ import react from "./src/images/react-icon.png"
 import reactNative from "./src/images/react-native-icon.png"
 import nft from "./src/images/NFT.png"
 
+import { Router, Location } from "@reach/router"
+import AllPost from "./src/pages/index"
+import ArticlePost from "./src/pages/post/[id]"
+import NotFoundPage from "./src/pages/404"
+
+import Dialog from "./src/stories/components/dialog/Dialog"
+
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
 const WrapPageElement = ({ element, props }) => (
   <Layout
@@ -19,30 +26,53 @@ const WrapPageElement = ({ element, props }) => (
     }}
     sidebarProps={{
       fixedItems: [
-        { IconComponent: CgMenuBoxed, text: "所有文章", value: "all" },
-        { IconComponent: FaFire, text: "熱門文章", value: "popular" },
+        { IconComponent: CgMenuBoxed, text: "所有文章", value: "/" },
+        { IconComponent: FaFire, text: "熱門文章", value: "/popular" },
       ],
       topicItems: [
-        { type: "title", text: "文章分類", value: "title" },
-        { type: "image", imageUrl: react, text: "React", value: "react" },
+        { type: "title", text: "文章分類", value: "/title" },
+        { type: "image", imageUrl: react, text: "React", value: "/react" },
         {
           type: "image",
           imageUrl: reactNative,
           text: "React Native",
-          value: "RN",
+          value: "/RN",
         },
-        { type: "image", imageUrl: nft, text: "NFT", value: "NFT" },
+        { type: "image", imageUrl: nft, text: "NFT", value: "/NFT" },
         {
           type: "image",
           imageUrl: imgTest,
           text: "個人網站",
-          value: "website",
+          value: "/website",
         },
       ],
     }}
     {...props}
   >
-    {element}
+    <Location>
+      {({ location, navigate }) => {
+        const { oldLocation } = location.state || {}
+
+        return (
+          <>
+            <Router location={oldLocation || location}>
+              <AllPost path="/" />
+              <ArticlePost path="post/:id" />
+              <NotFoundPage path="*" />
+            </Router>
+            {oldLocation && (
+              <Dialog
+                onClose={() => {
+                  navigate(oldLocation.pathname)
+                }}
+              >
+                <ArticlePost />
+              </Dialog>
+            )}
+          </>
+        )
+      }}
+    </Location>
   </Layout>
 )
 
