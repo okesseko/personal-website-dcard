7c840371e0c79b1c4e062a4a1ae24e6d9986ea5d
JImmy <59650459+okesseko@users.noreply.github.com>
2022-03-25 00:51:04 +0800

    feat: update css to match dcard layout

--- src/stories/components/header/Header.jsx
+++ src/stories/components/header/Header.jsx
@@ -16,56 +16,60 @@ const Header = ({ placeholder, onSearch }) => {
   const [inputValue, setInputValue] = useState("")
 
   return (
-    <header className="header">
-      <Link to="/">
-        <img height={28} src={logo} />
-      </Link>
-      <form className="header-searchbar">
-        <input
-          placeholder={placeholder}
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
-        />
-        {inputValue && (
-          <button
-            type="button"
-            className="header-searchbar__clean"
-            onClick={() => setInputValue("")}
-          >
-            <AiFillCloseCircle size={14} />
-          </button>
-        )}
-        <button
-          type="button"
-          className="header-searchbar__search"
-          onClick={() => onSearch(inputValue)}
+    <header className="header-wrapper">
+      <div className="header">
+        <Link to="/">
+          <img height={28} src={logo} />
+        </Link>
+        <form
+          className="header-searchbar"
+          onSubmit={e => {
+            e.preventDefault()
+            onSearch(inputValue)
+          }}
         >
-          <AiOutlineSearch size={20} />
-        </button>
-      </form>
-      <ul className="header-icon-group">
-        <li>
-          <a target="_blank" href="https://github.com/okesseko">
-            <AiFillGithub size={22} />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/yung-chen-lin-jimmy/"
-          >
-            <FaLinkedin size={22} />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=okesseko@gmail.com&su=Hi Jimmy, I am xxx&body=I am interesting about you🙌"
-          >
-            <IoMdMail size={22} />
-          </a>
-        </li>
-      </ul>
+          <input
+            placeholder={placeholder}
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+          />
+          {inputValue && (
+            <button
+              type="button"
+              className="header-searchbar__clean"
+              onClick={() => setInputValue("")}
+            >
+              <AiFillCloseCircle size={14} />
+            </button>
+          )}
+          <button type="submit" className="header-searchbar__search">
+            <AiOutlineSearch size={20} />
+          </button>
+        </form>
+        <ul className="header-icon-group">
+          <li>
+            <a target="_blank" href="https://github.com/okesseko">
+              <AiFillGithub size={22} />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/yung-chen-lin-jimmy/"
+            >
+              <FaLinkedin size={22} />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=okesseko@gmail.com&su=Hi Jimmy, I am xxx&body=I am interesting about you🙌"
+            >
+              <IoMdMail size={22} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
