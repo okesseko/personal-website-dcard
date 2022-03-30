3c0f07fd4fce1234eec76b07482a84a74c18bd1d
JImmy <59650459+okesseko@users.noreply.github.com>
2022-03-31 01:06:20 +0800

    feat: use category content to store category data

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
+import React from 'react'
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
@@ -9,6 +10,12 @@ import "./src/style.css"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
 
+import { CategoryProvide } from "./src/contents/CategoryContent"
+
 import CustomLayout from "./wrapPageElement"
 
 export const wrapPageElement = CustomLayout
+
+export const wrapRootElement = ({ element }) => (
+  <CategoryProvide>{element}</CategoryProvide>
+)
---
+++ src/contents/CategoryContent.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react"
+import { getCategory } from "@Api"
+
+const CategoryContent = React.createContext([])
+
+const CategoryProvide = ({ children }) => {
+  const [categoryList, setCategoryList] = useState([])
+
+  useEffect(() => {
+    getCategory()
+      .then(res => setCategoryList(res.data))
+      .catch(err => console.error(err))
+  }, [])
+
+  return (
+    <CategoryContent.Provider value={categoryList}>
+      {children}
+    </CategoryContent.Provider>
+  )
+}
+
+export default CategoryContent
+
+export { CategoryProvide }
