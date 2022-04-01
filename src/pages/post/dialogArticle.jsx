8a538801969d9c85cc749c2510309f880050e69f
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-01 23:00:55 +0800

    feat: add api

--- src/pages/post/dialogArticle.jsx
+++ src/pages/post/dialogArticle.jsx
@@ -1,48 +1,47 @@
 import { Link, navigate } from "gatsby"
 import React, { useEffect, useState } from "react"
 import { HiOutlineChevronLeft, HiOutlineChevronRight } from "react-icons/hi"
+import dayjs from "dayjs"
 
 import { getArticle } from "@Api"
 import Seo from "@Components/seo"
+import CategoryContent from "@Contents/CategoryContent"
 import Article from "@Components/article"
 import Intro from "@Components/intro"
 import avatar from "@Images/avatar.jpg"
 
+import getCategoryInfo from "@Utils/getCategoryInfo"
+
 import "./dialogArticle.scss"
 
-const MOCK_INFO = {
-  id: 2,
-  authImg: avatar,
-  articleImg: avatar,
-  category: "test",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged.",
-  emotionIcon: "🤣🥰🤩",
-  emotionNumber: 1,
-  releaseTime: "2020/01/01",
-  title: "Title",
-}
-
-const DialogArticle = ({ oldLocation, location, articleId, ...props }) => {
+const DialogArticle = ({ oldLocation, location, articleId }) => {
   const [maskDisplayed, setMaskDisplayed] = useState(false)
   const [article, setArticle] = useState({})
+  const [prevArticle, setPrevArticle] = useState(null)
+  const [nextArticle, setNextArticle] = useState(null)
 
   useEffect(() => {
     getArticleInfo()
   }, [articleId])
 
   function getArticleInfo() {
-    getArticle({ id: articleId })
+    getArticle({ id: articleId, getPrev: true, getNext: true })
       .then(res => {
+        console.log(res.data)
+        setPrevArticle(res.data.prevArticle)
         setArticle(res.data.articles[0])
+        setNextArticle(res.data.nextArticle)
       })
       .catch(err => {
         console.log(err)
       })
   }
 
-  const LinkArrow = ({ direction, articleInfo }) => {
-    return (
+  const LinkArrow = ({ direction, articleInfo, categoryList }) => {
+    const templateCategoryInfo =
+      getCategoryInfo(categoryList, articleInfo ? articleInfo.category : "") ||
+      {}
+    return articleInfo ? (
       <Link
         className="dialog-article__link"
         to={`/post/${articleInfo.id}`}
@@ -60,42 +59,65 @@ const DialogArticle = ({ oldLocation, location, articleId, ...props }) => {
         <div
           className={`dialog-article__link-intro dialog-article__link-intro--${direction}`}
         >
-          <Intro {...articleInfo} />
+          <Intro
+            authImg={avatar}
+            articleImg={articleInfo.previewImg}
+            category={templateCategoryInfo.name}
+            description={articleInfo.content}
+            emotionIcon={articleInfo.emotionIcon}
+            emotionNumber={articleInfo.emotionNumber}
+            releaseTime={dayjs(articleInfo.releaseTime).format("YYYY-MM-DD")}
+            title={articleInfo.title}
+          />
         </div>
       </Link>
+    ) : (
+      <div className="dialog-article__link--empty" />
     )
   }
 
   return (
     <>
-      <Seo title={`${article.title} - ${article.category} |Jimmy Lin`} />
-      <div className="dialog-article" onClick={e => e.stopPropagation()}>
-        <LinkArrow direction="left" articleInfo={MOCK_INFO} />
+      <Seo title={`${article.title} - ${article.category} | Jimmy Lin`} />
+      <CategoryContent.Consumer>
+        {categoryList => (
+          <div className="dialog-article" onClick={e => e.stopPropagation()}>
+            <LinkArrow
+              direction="left"
+              articleInfo={prevArticle}
+              categoryList={categoryList}
+            />
 
-        <div className="dialog-article__main">
-          <Article
-            article={article.content}
-            authImg={avatar}
-            category={{
-              text: article.category,
-              path: `/forum/${article.category}`,
-            }}
-            emotionIcon={article.emotionIcon}
-            emotionNumber={article.emotionNumber}
-            releaseTime={article.releaseTime}
-            title={article.title}
-            onClick={() => navigate("/")}
-          />
-        </div>
+            <div className="dialog-article__main">
+              <Article
+                article={article.content}
+                authImg={avatar}
+                category={{
+                  text: article.category,
+                  path: `/forum/${article.category}`,
+                }}
+                emotionIcon={article.emotionIcon}
+                emotionNumber={article.emotionNumber}
+                releaseTime={article.releaseTime}
+                title={article.title}
+                onClick={() => navigate("/")}
+              />
+            </div>
 
-        <LinkArrow direction="right" articleInfo={MOCK_INFO} />
+            <LinkArrow
+              direction="right"
+              articleInfo={nextArticle}
+              categoryList={categoryList}
+            />
 
-        <div
-          className={`dialog-article__mask ${
-            maskDisplayed && "dialog-article__mask--displayed"
-          }`}
-        />
-      </div>
+            <div
+              className={`dialog-article__mask ${
+                maskDisplayed && "dialog-article__mask--displayed"
+              }`}
+            />
+          </div>
+        )}
+      </CategoryContent.Consumer>
     </>
   )
 }
