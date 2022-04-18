455fbcbc08ddbb0927f73ea19f3eabd818d5d5c0
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-18 23:22:31 +0800

    feat: use category image for banner

--- src/pages/forum.jsx
+++ src/pages/forum.jsx
@@ -45,9 +45,7 @@ const Forum = ({ location, category }) => {
               introList={introList}
               location={location}
               onOrderChange={getIntroList}
-              bannerImg={
-                "https://megapx-assets.dcard.tw/images/c99966a1-03f9-4a69-86d4-df979a970496/full.jpeg"
-              }
+              bannerImg={templateCategoryInfo.image}
             />
           </div>
         )
